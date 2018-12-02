98c1d9ade46b756b82cfe59c34fb52dd84c57536
Giles Dring <gilesdring@gmail.com>
2018-12-02 22:11:20 +0000

    Add oauth negotiation middleware

---
+++ oauther/index.js
@@ -0,0 +1,46 @@
+const { promisify } = require('util')
+const writeFile = promisify(require('fs').writeFile)
+const { URL, URLSearchParams } = require('url')
+const express = require('express')
+const uuidv5 = require('uuid/v5')
+
+const app = express()
+const port = 8888
+
+const callbackUrl = 'http://oauth.test.dring.tech:8888/oauth/'
+
+function spotifyOauth (req, res, next) {
+  const spotifyState = uuidv5('http://oauth.test.dring.tech:8888/spotify', uuidv5.URL)
+  console.log(spotifyState)
+  const redirectUrl = new URL('/authorize', 'https://accounts.spotify.com')
+  redirectUrl.search = new URLSearchParams({
+    client_id: process.env.SPOTIFY_CLIENT_ID,
+    response_type: 'code',
+    redirect_uri: callbackUrl,
+    state: spotifyState,
+    show_dialog: true
+  }).toString()
+  // res.cookie('spotify_state', value )
+  res.redirect(redirectUrl)
+}
+
+async function oauthPage (req, res, next) {
+  const authFile = '../auth.json'
+  const authDetails = Object.assign({ callbackUrl: callbackUrl }, req.query)
+  console.log(authDetails)
+  try {
+    await writeFile(authFile, JSON.stringify(authDetails, null, 2))
+  } catch (e) {
+    console.error(e)
+  }
+  const pageContent = `
+  <p>Saved authentication details to ${authFile}</p>
+  <pre>${JSON.stringify(authDetails, null, 2)}</pre>
+  `
+  res.send(pageContent)
+}
+
+app.get('/spotify/', spotifyOauth)
+app.get('/oauth/', oauthPage)
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
